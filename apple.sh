d39c209fef84d20ef0393c4c878319f363004af2
Jayakrishna <69712032+chjayakrishnajk@users.noreply.github.com>
2024-07-14 10:07:00 +0530

    Added sh files

---
+++ apple.sh
@@ -0,0 +1,1 @@
+/home/jk/CaptiveUrl/bin/debug/net8.0/CaptiveUrl "http://captive.apple.com" "Success"
---
+++ msft.sh
@@ -0,0 +1,2 @@
+/home/jk/CaptiveUrl/bin/debug/net8.0/CaptiveUrl "http://www.msftconnecttest.com/connecttest.txt" "Microsoft Connect Test"
+
